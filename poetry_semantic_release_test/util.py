0a5b7bb64ddecef5696affe43d726b95afa677a0
Matteo Fordiani <matteofordiani@gmail.com>
2023-08-13 17:13:43 +0200

    feat: add sample package to release and test action

---
+++ poetry_semantic_release_test/util.py
@@ -0,0 +1,12 @@
+from typing import Callable
+from numpy import ndarray;
+
+def for_each(list: ndarray, function: Callable[[any, int, ndarray], None]):
+    for i, v in enumerate(list):
+        function(v, i, list)
+        
+def log(message: str):
+    print(message)
+    
+def say_hello(name: str):
+    print(f'Hello {name}!')
